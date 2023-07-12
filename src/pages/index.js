1ca8de2fe9ff80aebdc4b4219b4951a65ef45bc7
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-12 20:02:53 -0300

    Created container and subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Navbar from '@/components/navbar/navbar'
+import Subtitle from '@/components/tipography/subtitle/subtitle'
+import Container from '@/components/container/container'
 
 export default function Home() {
   return (
@@ -11,7 +13,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Navbar/>
+        <Navbar />
+        <Container>
+          <div>
+            <Subtitle>Promoções</Subtitle>
+          </div>
+          <div>
+            <Subtitle>Outros Jogos</Subtitle>
+          </div>
+        </Container>
       </div>
     </>
   )
