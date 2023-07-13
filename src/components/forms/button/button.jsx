3224467081154c73a8a3363ab8ccc6db6b66c8ae
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-12 22:45:51 -0300

    Created GameCard and Modified the button

--- src/components/forms/button/button.jsx
+++ src/components/forms/button/button.jsx
@@ -1,9 +1,9 @@
-import styles from './button.module.css'
+import styles from './button.module.css';
 
-export default function Button({children}){
-    return(
-        <button className={styles.button}>
+export default function Button({ children, fullWidth }) {
+    return (
+        <button className={`${styles.button} ${fullWidth && styles.fullwidth}`}>
             {children}
         </button>
-    )
+    );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
 import SaleCard from '@/components/cards/saleCard/saleCard'
 import styles from '@/styles/index.module.css'
+import GameCard from '@/components/cards/gameCard/gameCard'
 
 export default function Home() {
   return (
@@ -27,6 +28,9 @@ export default function Home() {
           </div>
           <div className={styles.session}>
             <Subtitle>Outros Jogos</Subtitle>
+            <div>
+              <GameCard/>
+            </div>
           </div>
         </Container>
       </div>
