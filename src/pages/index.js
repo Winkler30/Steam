b70f7eaf3c4fca197ebf717f3e9fe19fea4da291
charleswinkler76@gmail.com <charleswinkler76@gmail.com>
2023-07-15 16:04:46 -0300

    Created logic for the cart

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
@@ -7,6 +8,16 @@ import styles from '@/styles/index.module.css'
 import GameCard from '@/components/cards/gameCard/gameCard'
 
 export default function Home() {
+  const [cart, setCart] = useState([])
+
+  const handleAddProduct = (info) => {
+    setCart([...cart, info])
+  }
+  const handleRemoveProduct = (pos) => {
+    setCart(cart.filter((obj, posObj) => posObj !== pos ))
+  }
+
+  console.log(cart)
   return (
     <>
       <Head>
@@ -16,7 +27,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Navbar />
+        <Navbar cart={cart} onRemove={handleRemoveProduct}/>
         <Container>
           <div className={styles.session}>
             <Subtitle>Promoções</Subtitle>
@@ -26,28 +37,47 @@ export default function Home() {
                 discount='30%'
                 fullPrice='199,90'
                 discountPrice='99,90'
+                onAdd={() => handleAddProduct({
+                 name: 'League of Legends',
+                 price: 99.9,
+                 image: 'league-of-legends.jpg',
+                })}
               />
               <SaleCard
                 image={'hogwarts.jpg'}
                 discount='40%'
                 fullPrice='300,00'
                 discountPrice='249,99'
+                onAdd={() => handleAddProduct({
+                  name: 'Hogwarts',
+                  price: 259.9,
+                  image: 'hogwarts.jpg'
+                 })}
               />
               <SaleCard
                 image={'valorant.jpg'}
                 discount='50%'
                 fullPrice='399,90'
                 discountPrice='209,90'
+                onAdd={() => handleAddProduct({
+                  name: 'Valorant',
+                  price: 209.9,
+                  image: 'valorant.jpg'
+                 })}
+                
               />
             </div>
           </div>
           <div className={styles.session}>
             <Subtitle>Outros Jogos</Subtitle>
             <div className={styles.gamecontainer}>
-              <GameCard/>
-              <GameCard/>
-              <GameCard/>
-              <GameCard/>
+              <GameCard
+                onAdd={() => handleAddProduct({
+                  name: 'Counter-Strike',
+                  price: 99.9,
+                  image: 'counter-strike.jpg'
+                 })}
+              />
             </div>
           </div>
         </Container>
